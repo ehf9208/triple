365082ad74bc529a39a396d9c2e4c706b4bc2ccb
starbucks <starbucks@educo.co.kr>
2022-07-02 02:37:37 +0900

    테스트코드 주석

--- src/test/java/triple/ctr/ReviewCtrTest.java
+++ src/test/java/triple/ctr/ReviewCtrTest.java
@@ -69,66 +69,66 @@ public class ReviewCtrTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    public void eventModTest() throws Exception{
-        ReviewRequestBody vo = new ReviewRequestBody();
-        vo.setType("REVIEW");
-        vo.setAction("MOD");
-        vo.setReviewId("240a0658-dc5f-4878-9381-ebb7b266777");
-        vo.setContent("좋아쿵");
-        List<String> tmp = new ArrayList<>();
-        tmp.add("e4d1a64e-a531-46de-88d0-ff0ed70c0b111");
-        tmp.add("afb0cef2-851d-4a50-bb07-9cc15cbdc31234543");
-        vo.setAttachedPhotoIds(tmp);
-        vo.setUserId("3ede0ef2-92b7-4817-a5f3-0c575361f74");
-        vo.setPlaceId("2e4baf1c-5acb-4efb-a1af-eddada31b0");
-
-        List<String> deleteAttacheId = new ArrayList<>();
-        deleteAttacheId.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb");
-        vo.setDeleteAttacheId(deleteAttacheId);
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        String content = mapper.writeValueAsString(vo);
-        logger.debug("content = {}", content);
-
-        MockHttpServletRequestBuilder requestBuilder
-                = MockMvcRequestBuilders
-                .post ("/event")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
-                .accept(MediaType.APPLICATION_JSON);
-
-        this.mockMvc.perform(requestBuilder).andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void eventDeleteTest() throws Exception{
-        ReviewRequestBody vo = new ReviewRequestBody();
-        vo.setType("REVIEW");
-        vo.setAction("DELETE");
-        vo.setReviewId("240a0658-dc5f-4878-9381-ebb7b26677");
-        vo.setContent("좋아");
-        List<String> tmp = new ArrayList<>();
-        tmp.add("e4d1a64e-a531-46de-88d0-ff0ed70c0b");
-        tmp.add("afb0cef2-851d-4a50-bb07-9cc15cbdc3");
-        vo.setAttachedPhotoIds(tmp);
-        vo.setUserId("3ede0ef2-92b7-4817-a5f3-0c575361f74");
-        vo.setPlaceId("2e4baf1c-5acb-4efb-a1af-eddada31b0");
-
-        ObjectMapper mapper = new ObjectMapper();
-        String content = mapper.writeValueAsString(vo);
-        logger.debug("content = {}", content);
-
-        MockHttpServletRequestBuilder requestBuilder
-                = MockMvcRequestBuilders
-                .post ("/event")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
-                .accept(MediaType.APPLICATION_JSON);
-
-        this.mockMvc.perform(requestBuilder).andDo(print())
-                .andExpect(status().isOk());
-    }
+//    @Test
+//    public void eventModTest() throws Exception{
+//        ReviewRequestBody vo = new ReviewRequestBody();
+//        vo.setType("REVIEW");
+//        vo.setAction("MOD");
+//        vo.setReviewId("240a0658-dc5f-4878-9381-ebb7b266777");
+//        vo.setContent("좋아쿵");
+//        List<String> tmp = new ArrayList<>();
+//        tmp.add("e4d1a64e-a531-46de-88d0-ff0ed70c0b111");
+//        tmp.add("afb0cef2-851d-4a50-bb07-9cc15cbdc31234543");
+//        vo.setAttachedPhotoIds(tmp);
+//        vo.setUserId("3ede0ef2-92b7-4817-a5f3-0c575361f74");
+//        vo.setPlaceId("2e4baf1c-5acb-4efb-a1af-eddada31b0");
+//
+//        List<String> deleteAttacheId = new ArrayList<>();
+//        deleteAttacheId.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb");
+//        vo.setDeleteAttacheId(deleteAttacheId);
+//
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        String content = mapper.writeValueAsString(vo);
+//        logger.debug("content = {}", content);
+//
+//        MockHttpServletRequestBuilder requestBuilder
+//                = MockMvcRequestBuilders
+//                .post ("/event")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(content)
+//                .accept(MediaType.APPLICATION_JSON);
+//
+//        this.mockMvc.perform(requestBuilder).andDo(print())
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    public void eventDeleteTest() throws Exception{
+//        ReviewRequestBody vo = new ReviewRequestBody();
+//        vo.setType("REVIEW");
+//        vo.setAction("DELETE");
+//        vo.setReviewId("240a0658-dc5f-4878-9381-ebb7b26677");
+//        vo.setContent("좋아");
+//        List<String> tmp = new ArrayList<>();
+//        tmp.add("e4d1a64e-a531-46de-88d0-ff0ed70c0b");
+//        tmp.add("afb0cef2-851d-4a50-bb07-9cc15cbdc3");
+//        vo.setAttachedPhotoIds(tmp);
+//        vo.setUserId("3ede0ef2-92b7-4817-a5f3-0c575361f74");
+//        vo.setPlaceId("2e4baf1c-5acb-4efb-a1af-eddada31b0");
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        String content = mapper.writeValueAsString(vo);
+//        logger.debug("content = {}", content);
+//
+//        MockHttpServletRequestBuilder requestBuilder
+//                = MockMvcRequestBuilders
+//                .post ("/event")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(content)
+//                .accept(MediaType.APPLICATION_JSON);
+//
+//        this.mockMvc.perform(requestBuilder).andDo(print())
+//                .andExpect(status().isOk());
+//    }
 }
